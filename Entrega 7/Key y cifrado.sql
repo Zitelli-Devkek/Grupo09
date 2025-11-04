5c74b5925875cfeb2b2380ac42c29ed12d4cb4a8
mateofeiertag <mateofeiertag@gmail.com>
2025-11-04 00:04:51 -0300

    Update and rename Encriptacion to Key y cifrado.sql

---
+++ Entrega 7/Key y cifrado.sql
@@ -0,0 +1,84 @@
+/*
+-- =================================================================
+-- Asignatura: Bases de Datos Aplicada
+-- Comisión: 01-2900 
+-- Grupo Nro: 09
+-- Fecha de Entrega:
+--
+-- Enunciado: Encriptacion
+-- =================================================================
+-- Integrantes:
+-- Jiménez Damián (DNI 43.194.984)
+-- Mendoza Gonzalo (DNI 44.597.456)
+-- Demis Colman (DNI 37.174.947)
+-- Feiertag Mateo (DNI 46.293.138)
+-- Suriano Lautaro (DNI 44.792.129)
+-- Zitelli Emanuel (DNI 45.064.107)
+-- =================================================================
+*/
+
+
+USE AltosSaintJust; 
+GO
+
+BEGIN
+    CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'password09';
+END
+GO
+
+
+BEGIN
+    CREATE CERTIFICATE Cert_DatosSensibles WITH SUBJECT = 'Certificado para cifrado de datos personales';
+END
+GO
+
+
+BEGIN
+    CREATE SYMMETRIC KEY SK_DatosSensibles
+    ENCRYPTION BY CERTIFICATE Cert_DatosSensibles;
+END
+GO
+
+
+
+
+
+OPEN SYMMETRIC KEY SK_DatosSensibles
+DECRYPTION BY CERTIFICATE Cert_DatosSensibles;
+GO
+
+-- --- Cifrar Tabla [Persona] ---
+
+
+ALTER TABLE Persona ADD DNI_Enc VARBINARY(256);
+ALTER TABLE Persona ADD Email_Enc VARBINARY(256);
+ALTER TABLE Persona ADD Telefono_Enc VARBINARY(256);
+ALTER TABLE Persona ADD CVU_CBU_Enc VARBINARY(256);
+GO
+
+
+UPDATE Propietario
+SET 
+    DNI_Enc = ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), DNI),
+    Email_Enc = ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), email_personal),
+    Telefono_Enc = ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), telefono),
+    CVU_CBU_Enc = ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), cbu_cvu);
+GO
+
+
+ALTER TABLE Persona DROP COLUMN DNI;
+ALTER TABLE Persona DROP COLUMN email_personal;
+ALTER TABLE Persona DROP COLUMN Telefono;
+ALTER TABLE Persona DROP COLUMN CVU_CBU;
+GO
+
+
+EXEC sp_rename 'Propietario.DNI_Enc', 'DNI', 'COLUMN';
+EXEC sp_rename 'Propietario.Email_Enc', 'Email', 'COLUMN';
+EXEC sp_rename 'Propietario.Telefono_Enc', 'Telefono', 'COLUMN';
+EXEC sp_rename 'Propietario.CVU_CBU_Enc', 'CVU_CBU', 'COLUMN';
+GO
+
+CLOSE SYMMETRIC KEY SK_DatosSensibles;
+GO
+
