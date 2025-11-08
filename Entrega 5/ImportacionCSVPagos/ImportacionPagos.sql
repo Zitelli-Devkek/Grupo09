6e3eac1e0886e603b03fb5b244bce7791f799c9a
shadowcode23 <damian.nahuel15@gmail.com>
2025-11-08 11:35:40 -0300

    agregado de datos integrantes

--- Entrega 5/ImportacionCSVPagos/ImportacionPagos.sql
+++ Entrega 5/ImportacionCSVPagos/ImportacionPagos.sql
@@ -1,3 +1,19 @@
+/*
+BASE DE DATOS APLICADA
+
+GRUPO 9
+
+Alumnos:
+Jiménez Damián (DNI 43.194.984)
+Mendoza Gonzalo (DNI 44.597.456)
+Demis Colman (DNI 37.174.947)
+Feiertag Mateo (DNI 46.293.138)
+Suriano Lautaro (DNI 44.792.129)
+Zitelli Emanuel (DNI 45.064.107)
+
+SP para importacion de pagos_consorcios.csv
+*/
+
 USE Com2900G09
 GO
 
