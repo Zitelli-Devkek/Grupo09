f2d4dbd3a65b473a146d22b041249f5c059b7d60
mateofeiertag <mateofeiertag@gmail.com>
2025-11-10 03:53:33 -0300

    Update Cifrado sp_Persona.sql

--- Entrega 7/Cifrado sp_Persona.sql
+++ Entrega 7/Cifrado sp_Persona.sql
@@ -17,49 +17,100 @@
 -- =================================================================
 */
 
+USE Com2900G09;
+GO
+
 CREATE OR ALTER PROCEDURE sp_importar_csv_inquilino_propietarios_datos
+    @rutaArchivo NVARCHAR(500) 
 AS
 BEGIN
     SET NOCOUNT ON;
 
+--
+    IF OBJECT_ID('tempdb..#inquilino_propietarios_datos_tmp') IS NOT NULL
+        DROP TABLE #inquilino_propietarios_datos_tmp;
+
+    CREATE TABLE #inquilino_propietarios_datos_tmp (
+        nombre NVARCHAR(100),
+        apellido NVARCHAR(100),
+        DNI NVARCHAR(50),
+        email_personal NVARCHAR(100),
+        telefono NVARCHAR(50),
+        cbu_cvu NVARCHAR(22),
+        inquilino NVARCHAR(10)
+    );
+
     BEGIN TRY
+        DECLARE @sql NVARCHAR(MAX);
+        SET @sql = N'
+            BULK INSERT #inquilino_propietarios_datos_tmp
+            FROM ''' + @rutaArchivo + N'''
+            WITH (
+                FIRSTROW = 2,
+                FIELDTERMINATOR = '';'',
+                ROWTERMINATOR = ''\n'',
+                CODEPAGE = ''ACP''
+            );
+        ';
+        EXEC sp_executesql @sql;
+
+        INSERT INTO Tipo_Ocupante (descripcion)
+SELECT 
+    CASE 
+        WHEN TRY_CONVERT(INT, inquilino) = 1 THEN 'Inquilino'
+        WHEN TRY_CONVERT(INT, inquilino) = 0 THEN 'Propietario'
+    END
+FROM #inquilino_propietarios_datos_tmp
+WHERE TRY_CONVERT(INT, inquilino) IN (0, 1)
+
+        --  CIFRAR Y GUARDAR 
+
         OPEN SYMMETRIC KEY SK_DatosSensibles 
         DECRYPTION BY CERTIFICATE Cert_DatosSensibles;
 
-        MERGE INTO persona AS Target
-        USING sp_importar_csv_persona AS Source
-        ON (Target.IdPersona = Source.IdPersona)
-
-        WHEN MATCHED THEN
-            UPDATE SET
-                Target.Nombre = Source.Nombre,
-                Target.Apellido = Source.Apellido,
-                Target.DNI = ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), Source.DNI),
-                Target.email_personal = ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), Source.email_personal),
-                Target.telefono = ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), Source.telefono),
-                Target.cbu_cvu = ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), Source.cbu_cvu)
-
-        WHEN NOT MATCHED BY TARGET THEN
-            INSERT (IdPersona, Nombre, Apellido, DNI, Email, Telefono, CVU_CBU)
-            VALUES (
-                Source.IdPersona, Source.Nombre, Source.Apellido,
-                ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), Source.DNI),
-                ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), Source.email_personal),
-                ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), Source.telefono),
-                ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), Source.cbu_cvu)
-            );
-
-
-        CLOSE SYMMETRIC KEY SK_DatosSensibles;
+        INSERT INTO Persona (
+            DNI, --  cifrada
+            id_tipo_ocupante, 
+            nombre, 
+            apellido, 
+            email_personal, --  cifrada
+            telefono, --  cifrada
+            cbu_cvu --  cifrada
+        )
+        SELECT 
+            --  Cifrar los datos 
+            ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), TRY_CONVERT(CHAR(8), t.DNI)),
+            1, -- tipo_ocupante
+            t.nombre,
+            t.apellido,
+            ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), t.email_personal),
+            ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), t.telefono),
+            ENCRYPTBYKEY(KEY_GUID('SK_DatosSensibles'), t.cbu_cvu)
+        FROM (
+            SELECT *,
+                   ROW_NUMBER() OVER (PARTITION BY TRY_CONVERT(CHAR(8), DNI) ORDER BY (SELECT NULL)) AS rn
+            FROM #inquilino_propietarios_datos_tmp
+        ) AS t
+        WHERE rn = 1
+          AND TRY_CONVERT(INT, t.DNI) BETWEEN 10000000 AND 99999999
+          AND NOT EXISTS (
+                -- Descifrar el DNI  para comparar
+                SELECT 1 FROM Persona AS p 
+                WHERE CONVERT(VARCHAR, DECRYPTBYKEY(p.DNI)) = TRY_CONVERT(CHAR(8), t.DNI)
+          );
         
+        -- Cerrar la llave
+        CLOSE SYMMETRIC KEY SK_DatosSensibles;
+
 
     END TRY
     BEGIN CATCH
+        -- Asegurarse de cerrar la llave si hay un error
         IF (SELECT key_guid('SK_DatosSensibles') FROM sys.open_keys) IS NOT NULL
             CLOSE SYMMETRIC KEY SK_DatosSensibles;
         
-        PRINT 'Error: No se pudo procesar el staging cifrado';
+        PRINT 'Error: No se pudo importar el archivo .csv';
         THROW;
     END CATCH
-END
+END;
 GO
