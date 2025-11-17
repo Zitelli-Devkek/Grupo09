62d1720bed9a75fa8cce22e02e7d5915231b442c
shadowcode23 <damian.nahuel15@gmail.com>
2025-11-17 18:33:18 -0300

    Update 02_CargaArchivos.sql

--- Entrega 5/02_CargaArchivos.sql
+++ Entrega 5/02_CargaArchivos.sql
@@ -373,6 +373,22 @@ BEGIN
     END CATCH
 END;
 
+/*
+BASE DE DATOS APLICADA
+
+GRUPO 9
+
+Alumnos:
+Jiménez Damián (DNI 43.194.984)
+Mendoza Gonzalo (DNI 44.597.456)
+Demis Colman (DNI 37.174.947)
+Feiertag Mateo (DNI 46.293.138)
+Suriano Lautaro (DNI 44.792.129)
+Zitelli Emanuel (DNI 45.064.107)
+
+SP para importar "Servicios.Servicios.json" en la tabla Servicio
+*/
+
 CREATE OR ALTER PROCEDURE sp_ImportarServicios
     @RutaArchivo NVARCHAR(500)
 AS
