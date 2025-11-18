577e9ac5ca6133047ffe209ddac52c6192ce93f9
mateofeiertag <mateofeiertag@gmail.com>
2025-11-18 16:39:15 -0300

    Update LogIns.sql

--- Entrega 7/LogIns.sql
+++ Entrega 7/LogIns.sql
@@ -29,3 +29,32 @@ BEGIN
         WITH PASSWORD = 'password_sistemas';
 END
 GO
+
+-- ---   CREACION DE USUARIOS ---
+USE Com2900G09;
+GO
+
+    
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_general_1')
+BEGIN
+    CREATE USER [usuario_general_1] FOR LOGIN [login_general_1];
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_bancario_1')
+BEGIN
+    CREATE USER [usuario_bancario_1] FOR LOGIN [login_bancario_1];
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_operativo_1')
+BEGIN
+    CREATE USER [usuario_operativo_1] FOR LOGIN [login_operativo_1];
+END
+GO
+    
+IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_sistemas_1')
+BEGIN
+    CREATE USER [usuario_sistemas_1] FOR LOGIN [login_sistemas_1];
+END
+GO
