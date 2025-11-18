15b305961a4e707b8d1cd53939a9f748ddb848df
mateofeiertag <mateofeiertag@gmail.com>
2025-11-18 20:12:04 -0300

    Create Users.sql

---
+++ Entrega 7/Users.sql
@@ -0,0 +1,24 @@
+USE Com2900G09;
+GO
+
+CREATE OR ALTER PROCEDURE dbo.sp_crear_usuarios
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    
+    IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_general_1')
+        CREATE USER [usuario_general_1] FOR LOGIN [login_general_1];
+    
+    IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_bancario_1')
+        CREATE USER [usuario_bancario_1] FOR LOGIN [login_bancario_1];
+    
+    IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_operativo_1')
+        CREATE USER [usuario_operativo_1] FOR LOGIN [login_operativo_1];
+        
+    IF NOT EXISTS (SELECT * FROM sys.database_principals WHERE name = 'usuario_sistemas_1')
+        CREATE USER [usuario_sistemas_1] FOR LOGIN [login_sistemas_1];
+
+
+END
+GO
