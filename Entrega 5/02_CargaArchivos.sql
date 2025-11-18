729f992264b5e4ae855c768cd5bf386eb99aabf9
Zitelli-Devkek <82792113+Zitelli-Devkek@users.noreply.github.com>
2025-11-18 18:28:32 -0300

    Agrego un stored procedure para importar un csv que ayuda a cargar Persona_UF

--- Entrega 5/02_CargaArchivos.sql
+++ Entrega 5/02_CargaArchivos.sql
@@ -606,7 +606,84 @@ BEGIN
     END CATCH
 END;
 GO
+/*
+/********************************************************************
+Consigna: En este script se importa el archivo inquilino-propietarios-UF.csv y se carga la tabla "Persona_Uf"
+*********************************************************************/
+*/
+CREATE OR ALTER PROCEDURE sp_importar_csv_inquilino_propietarios_UF
+    @rutaArchivo NVARCHAR(500)
+AS
+BEGIN
+    SET NOCOUNT ON;
 
+    -- Si existe la tabla temporal la elimino. Para que no tire error al ejecutar dos veces
+    IF OBJECT_ID('tempdb..#inquilino_propietarios_UF_tmp') IS NOT NULL
+        DROP TABLE #inquilinopropietariosUFtmp;
+
+    CREATE TABLE #inquilino_propietarios_UF_tmp (
+        cbu_cvu NVARCHAR(22),
+        nombre NVARCHAR(100),
+        nr_uf NVARCHAR(50),
+        piso NVARCHAR(100),
+        departamento NVARCHAR(50)
+    );
+
+    -- Me fijo si existe el archivo a importar
+    DECLARE @existe INT;
+    EXEC master.dbo.xp_fileexist @rutaArchivo, @existe OUTPUT;
+
+    IF @existe = 0
+    BEGIN
+        RAISERROR('Escribiste mal la ruta, o el archivo no existe.', 16, 1);
+        RETURN;
+    END
+
+    -- Inicio una transacción. O cargamos todo o no cargamos nada
+    BEGIN TRY
+        BEGIN TRAN;
+
+        DECLARE @sql NVARCHAR(MAX);
+        SET @sql = N'
+            BULK INSERT #inquilino_propietarios_UF_tmp
+            FROM ''' + @rutaArchivo + N'''
+            WITH (
+                FIRSTROW = 2,
+                FIELDTERMINATOR = ''|'',
+                ROWTERMINATOR = ''\n'',
+                CODEPAGE = ''ACP''
+            );
+        ';
+        EXEC sp_executesql @sql;
+       
+
+        -- Cargo los registros en la tabla Persona_uf, filtrando repetidos en la consulta
+
+INSERT INTO Persona_UF (DNI, id_uf)
+SELECT 
+    p.DNI,
+    uf.id_uf
+FROM #inquilino_propietarios_UF_tmp AS c --la tabla temporal carga todo bien
+JOIN Persona AS p
+      ON p.cbu_cvu = c.cbu_cvu --hasta acá tiene que estar todo bien
+JOIN Consorcio AS cons
+      ON cons.nombre = c.nombre
+JOIN Unidad_Funcional AS uf
+      ON uf.id_consorcio = cons.id_consorcio
+     AND uf.nr_uf        = c.nr_uf
+     AND uf.piso         = c.piso
+     AND uf.departamento = c.departamento;
+        COMMIT TRAN;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRAN;
+        PRINT 'Error: Lo siento, no se pudo importar el archivo .csv';
+        PRINT ERROR_MESSAGE();
+        THROW;
+    END CATCH
+END;
+GO
 /********************************************************************
 Consigna: En este script se importa el archivo inquilino-propietarios-UF.csv y se carga la tabla "Persona_Uf"
 *********************************************************************/
