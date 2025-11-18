f5cac462364bee498c217f5b5f666d6e4f720f91
mateofeiertag <mateofeiertag@gmail.com>
2025-11-18 15:42:36 -0300

    Update LogIns.sql

--- Entrega 7/LogIns.sql
+++ Entrega 7/LogIns.sql
@@ -4,24 +4,28 @@ GO
 
 IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'login_general_1')
 BEGIN
-    CREATE LOGIN [login_general_1] WITH PASSWORD = N'password_general';
+    CREATE LOGIN login_general_1 
+        WITH PASSWORD = 'password_general';
 END
 GO
 
 IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'login_bancario_1')
 BEGIN
-    CREATE LOGIN [login_bancario_1] WITH PASSWORD = N'password_bancario';
+    CREATE LOGIN login_bancario_1 
+        WITH PASSWORD = 'password_bancario';
 END
 GO
 
 IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'login_operativo_1')
 BEGIN
-    CREATE LOGIN [login_operativo_1] WITH PASSWORD = N'password_operativo';
+    CREATE LOGIN login_operativo_1 
+        WITH PASSWORD = 'password_operativo';
 END
 GO
 
 IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'login_sistemas_1')
 BEGIN
-    CREATE LOGIN [login_sistemas_1] WITH PASSWORD = N'password_sistemas';
+    CREATE LOGIN login_sistemas_1 
+        WITH PASSWORD = 'password_sistemas';
 END
 GO
