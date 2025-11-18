7ad263baaf95550ac573fded2eb107ceffa3bb49
mateofeiertag <mateofeiertag@gmail.com>
2025-11-18 20:15:51 -0300

    Update LogIns.sql

--- Entrega 7/LogIns.sql
+++ Entrega 7/LogIns.sql
@@ -1,35 +1,34 @@
 -- Esto se ejecuta en la base de datos 'master'
 
 
-USE master; 
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'login_general_1')
+CREATE OR ALTER PROCEDURE dbo.sp_crear_logins
+AS
 BEGIN
-    CREATE LOGIN login_general_1 
-        WITH PASSWORD = 'password_general';
+    SET NOCOUNT ON;
+    
+    DECLARE @SQL NVARCHAR(MAX);
+    
+    SET @SQL = N'
+    
+    -- Los comandos CREATE LOGIN deben ejecutarse en un contexto seguro y con permisos elevados.
+
+    IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = N''login_general_1'')
+        CREATE LOGIN login_general_1 WITH PASSWORD = ''password_general'', CHECK_POLICY = OFF;
+    
+    IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = N''login_bancario_1'')
+        CREATE LOGIN login_bancario_1 WITH PASSWORD = ''password_bancario'', CHECK_POLICY = OFF;
+        
+    IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = N''login_operativo_1'')
+        CREATE LOGIN login_operativo_1 WITH PASSWORD = ''password_operativo'', CHECK_POLICY = OFF;
+        
+    IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = N''login_sistemas_1'')
+        CREATE LOGIN login_sistemas_1 WITH PASSWORD = ''password_sistemas'', CHECK_POLICY = OFF;
+    ';
+    
+    -- La ejecución de este SP requiere el permiso 'ALTER ANY LOGIN' o ser un sysadmin.
+    EXEC sys.sp_executesql @SQL;
+    
+    PRINT 'Logins de servidor creados o verificados.';
 END
 GO
-
-IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'login_bancario_1')
-BEGIN
-    CREATE LOGIN login_bancario_1 
-        WITH PASSWORD = 'password_bancario';
-END
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'login_operativo_1')
-BEGIN
-    CREATE LOGIN login_operativo_1 
-        WITH PASSWORD = 'password_operativo';
-END
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = 'login_sistemas_1')
-BEGIN
-    CREATE LOGIN login_sistemas_1 
-        WITH PASSWORD = 'password_sistemas';
-END
-GO
-
 
